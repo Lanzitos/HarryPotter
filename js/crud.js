70686ffb62935ad60057691488cca11923a5694b
Alan Santos <alansantos04072@gmail.com>
2025-05-30 13:26:04 -0300

    santinha

--- js/crud.js
+++ js/crud.js
@@ -3,17 +3,42 @@ let filmes = document.querySelector('.card-container')
 
 function testeApi(filme){
     return `
-        <li>
-            <div class="card"><p onclick="filmeFavorito()"><button class="estrela">&#9733;</button></p>
-                <img src="${filme.poster}" alt="">
-                <h2>${filme.title}</h2>
-                <p></p>
-            </div>
+        <li>    
+        <div class="card"><p><button class="estrela" onclick="salvar()">&#9733;</button></p>
+        <img src="${filme.poster}" alt="">
+        <h2>${filme.title}</h2>
+        </div>
         </li>
-    `
+        `
 }
 
+function salvar(){
+    const estrelas = document.querySelectorAll('.estrela')
+    estrelas.forEach(estrelas => {
+    
+    estrelas.addEventListener('click', () => {
+    const estaFavorito = estrelas.classList.contains('favorito');
+    if (estaFavorito) {
+      estrelas.classList.remove('favorito')
+      estrelas.style.color = 'yellow'
+    } else {
+      estrelas.classList.add('favorito')
+      estrelas.style.color = 'black'
+    }
+  })
+}
+
+)}        
+
+
+
+
+
 fetch(url)
-    .then((response) => response.json())
-    .then(jsonresponse => filmes.innerHTML = jsonresponse.map(testeApi).join(""))
-    .catch((error) => console.log(error))
+.then((response) => response.json())
+.then(jsonresponse => filmes.innerHTML = jsonresponse.map(testeApi).join(""))
+.catch((error) => console.log(error))
+
+
+
+
