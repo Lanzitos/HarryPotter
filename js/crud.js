b11b72fd99df29e1c4d22faad31f88304b73e550
Alan Santos <alansantos04072@gmail.com>
2025-05-30 21:58:06 -0300

    Update crud.js

--- js/crud.js
+++ js/crud.js
@@ -5,7 +5,7 @@ function testeApi(filme){
     return `
         <li>    
         <div class="card">
-        <p><button class="estrela">&#9733;
+        <p><button class="estrela" onclick="salvar()">&#9733;
         </button></p>
         <img src="${filme.poster}" alt="${filme.title}">
         <h2>${filme.title}</h2>
@@ -36,5 +36,6 @@ fetch(url)
 .then((response) => response.json())
 .then(jsonresponse => filmes.innerHTML = jsonresponse.map(testeApi).join(""))
 .catch((error) => console.log(error))
+
 
 
